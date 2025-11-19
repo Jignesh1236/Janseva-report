444c2b28ef473f2e7d4b51119d0bf7206601d449
Jignesg D Maru <jigneshmaru690@gmail.com>
2025-11-19 12:53:22 +0530

    Update Navigation.tsx tm

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -71,7 +71,7 @@ const Navigation: React.FC<NavigationProps> = ({ language, translations, setLang
         <div className="flex justify-between items-center h-16">
           <Link href="/" className="flex items-center space-x-2 group">
             <div className="relative">
-              <span className="absolute -top-1 -right-1 text-[10px] font-semibold text-primary transform scale-110 select-none">®</span>
+              <span className="absolute -top-1 -right-1 text-[10px] font-semibold text-primary transform scale-110 select-none">TM</span>
               <Image
                 src="/icons/image-removebg-preview (1).png"
                 alt="Janseva Kendra (Private) Logo"
